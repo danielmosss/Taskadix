037b6bf119f6815e34984232b66092b45febf4fd
WORKSTATION-DM <daniel.mosselman@outlook.com>
2023-11-27 19:20:36 +0100

    Removed sample date insert

--- database/todo.sql
+++ database/todo.sql
@@ -25,16 +25,3 @@ BEGIN
     SELECT LAST_INSERT_ID() as id;
 END $$
 DELIMITER ;
-
-
--- insert some sample data
-call insertAtodoTask('Grocery Shopping', 'Buy groceries for the week', '2023-11-26');
-call insertAtodoTask('Book Club Meeting', 'Discuss the latest book with the group', '2023-11-26');
-call insertAtodoTask('Yoga Class', 'Attend the morning yoga session at the local gym', '2023-11-27');
-call insertAtodoTask('Client Follow-Up', 'Check in with clients regarding their feedback', '2023-11-27');
-call insertAtodoTask('Report Submission', 'Submit the monthly financial report', '2023-11-28');
-call insertAtodoTask('Team Sync-Up', 'Weekly team meeting to discuss project progress', '2023-11-29');
-call insertAtodoTask('Dentist Appointment', 'Routine dental check-up', '2023-11-29');
-call insertAtodoTask('Car Service', 'Take the car to the dealership for scheduled maintenance', '2023-11-30');
-call insertAtodoTask('Parent-Teacher Conference', 'Meet with the kids’ teachers to discuss academic performance', '2023-11-30');
-call insertAtodoTask('Networking Event', 'Attend local business networking event', '2023-12-01');
