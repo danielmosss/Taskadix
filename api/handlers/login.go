7cef6c5f3de831025f5f349e3232178234362cb3
WORKSTATION-DM <daniel.mosselman@outlook.com>
2023-12-04 21:46:15 +0100

    Add login functionality to API handler

--- api/handlers/login.go
+++ api/handlers/login.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"api/functions"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -10,9 +11,54 @@ type LoginResponse struct {
 	JsonWebToken string `json:"jsonwebtoken"`
 }
 
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func Login(res http.ResponseWriter, req *http.Request) {
-	
-	jwtToken, err := functions.CreateToken(1)
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var loginRequest LoginRequest
+	if err := json.Unmarshal(body, &loginRequest); err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	dbConnection, err := functions.GetDatabaseConnection()
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	query := "SELECT id FROM users WHERE username = ? AND password = ?;"
+	result, err := dbConnection.Query(query, loginRequest.Username, loginRequest.Password)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer result.Close()
+	defer dbConnection.Close()
+
+	var id int
+	for result.Next() {
+		err := result.Scan(&id)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if id == 0 {
+		http.Error(res, "User not found", http.StatusNotFound)
+		return
+	}
+
+	jwtToken, err := functions.CreateToken(id)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 		return
