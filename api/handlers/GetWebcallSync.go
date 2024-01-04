bac61cb5ea530f646cc154861a1ee9bdafa15a8d
Daniël M <daniel.mosselman@outlook.com>
2024-01-04 07:22:40 +0100

    Add GetWebcallSync and PostWebcallUrl handlers

---
+++ api/handlers/GetWebcallSync.go
@@ -0,0 +1,71 @@
+package handlers
+
+import (
+	"api/functions"
+	"database/sql"
+	"encoding/json"
+	"net/http"
+)
+
+type userData struct {
+	Username          string `json:"username"`
+	Email             string `json:"email"`
+	Webcallurl        string `json:"webcallurl"`
+	Webcalllastsynced string `json:"webcalllast"`
+}
+
+func GetUserData(res http.ResponseWriter, req *http.Request) {
+	userId, err := functions.GetUserID(req)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	dbConnection, err := functions.GetDatabaseConnection()
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	query := "SELECT username, email, webcallurl, webcalllastsynced FROM users WHERE id = ?;"
+	result, err := dbConnection.Query(query, userId)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer result.Close()
+	defer dbConnection.Close()
+
+	var userdata userData
+	for result.Next() {
+		var webcallurl sql.NullString
+		var webcalllastsynced sql.NullString
+
+		// webcallurl and webcalllastsynced are nullable, so we need to check for null values
+		err := result.Scan(&userdata.Username, &userdata.Email, &webcallurl, &webcalllastsynced)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if webcallurl.Valid {
+			userdata.Webcallurl = webcallurl.String
+		} else {
+			userdata.Webcallurl = ""
+		}
+		if webcalllastsynced.Valid {
+			userdata.Webcalllastsynced = webcalllastsynced.String
+		} else {
+			userdata.Webcalllastsynced = ""
+		}
+	}
+
+	jsondata, err := json.Marshal(userdata)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	res.Write(jsondata)
+}
