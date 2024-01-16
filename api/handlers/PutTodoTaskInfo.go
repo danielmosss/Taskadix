d42c35d6a863f4689f98a46ad6fb8a591bb4d0a7
Daniël M <daniel.mosselman@outlook.com>
2024-01-16 09:42:16 +0100

    Add date picker to cardpopup component and update task date in PutTodoTaskInfo

--- api/handlers/PutTodoTaskInfo.go
+++ api/handlers/PutTodoTaskInfo.go
@@ -32,6 +32,14 @@ func PutTodoTaskInfo(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	querySelectOldDate := "SELECT date FROM todos WHERE id = ? AND userId = ?;"
+	resultOldDate, err := dbConnection.Query(querySelectOldDate, newTask.Id, userId)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resultOldDate.Close()
+
 	// Update the title or description of a task
 	query := "UPDATE todos SET title = ?, description = ? WHERE id = ? AND userId = ?;"
 	result, err := dbConnection.Query(query, newTask.Title, newTask.Description, newTask.Id, userId)
@@ -40,5 +48,40 @@ func PutTodoTaskInfo(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	defer result.Close()
+
+	var oldDate string
+	for resultOldDate.Next() {
+		err := resultOldDate.Scan(&oldDate)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if oldDate != newTask.Date {
+		querySelectMaxTodoOrder := "SELECT MAX(todoOrder) FROM todos WHERE userId = ? AND date = ?;"
+		resultMaxTodoOrder, err := dbConnection.Query(querySelectMaxTodoOrder, userId, newTask.Date)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var maxTodoOrder int
+		for resultMaxTodoOrder.Next() {
+			err := resultMaxTodoOrder.Scan(&maxTodoOrder)
+			if err != nil {
+				http.Error(res, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		queryUpdateDateAndTodoOrder := "UPDATE todos SET date = ?, todoOrder = ? WHERE id = ? AND userId = ?;"
+		resultUpdateDateAndTodoOrder, err := dbConnection.Query(queryUpdateDateAndTodoOrder, newTask.Date, maxTodoOrder+1, newTask.Id, userId)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer resultUpdateDateAndTodoOrder.Close()
+	}
 	defer dbConnection.Close()
 }
