9faff3f874469275d2d448767300a4e3f5cbf588
Daniël M <121039995+danielmosss@users.noreply.github.com>
2024-03-19 21:48:21 +0100

    Refactor week overview component layout and styling

--- angular/src/app/pages/week-overview/week-overview.component.ts
+++ angular/src/app/pages/week-overview/week-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateAppointmentComponent } from 'src/app/popups/create-appointment/create-appointment.component';
 
@@ -9,6 +9,8 @@ export interface WeekTask {
   day: string; // 'Monday', 'Tuesday', etc.
   startTime: string; // Bijvoorbeeld '10:00'
   endTime: string; // Bijvoorbeeld '11:00'
+  width?: number;
+  left?: number;
 }
 
 
@@ -17,14 +19,29 @@ export interface WeekTask {
   templateUrl: './week-overview.component.html',
   styleUrls: ['./week-overview.component.scss']
 })
-export class WeekOverviewComponent {
+export class WeekOverviewComponent implements OnInit{
+  public weeknumber: number = 0;
+  public heightPerHour: number = 60;
+  public dividerHeight: number = 2;
 
   constructor(private _dialog: MatDialog) { }
 
-  weekTasks: WeekTask[] = [
+  ngOnInit(): void {
+    this.weekAppointments = this.calculateOverlaps(this.weekAppointments);
+  }
+
+  weekAppointments: WeekTask[] = [
     {
       id: '1',
       title: 'Meeting',
+      description: 'Meeting with the team',
+      day: 'Monday',
+      startTime: '10:00',
+      endTime: '11:00'
+    },
+    {
+      id: '3',
+      title: 'Meeting 2',
       description: 'Meeting with the team',
       day: 'Monday',
       startTime: '10:00',
@@ -56,20 +73,31 @@ export class WeekOverviewComponent {
     return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   }
 
-  // getTaskStyle(task: WeekTask): any {
-  //   const startHour = parseInt(task.startTime.split(':')[0], 10);
-  //   const startMinute = parseInt(task.startTime.split(':')[1], 10);
-  //   const endHour = parseInt(task.endTime.split(':')[0], 10);
-  //   const endMinute = parseInt(task.endTime.split(':')[1], 10);
+  calculateOverlaps(tasks: WeekTask[]): WeekTask[] {
+    const sortedTasks = tasks.sort((a, b) => a.startTime.localeCompare(b.startTime));
 
-  //   const startRow = (startHour - 7) * 2 + (startMinute / 30) + 1; // Adjusted for 7:00 start and 2 rows per hour
-  //   const endRow = (endHour - 7) * 2 + (endMinute / 30) + 1; // Adjusted for 7:00 start and 2 rows per hour
+    for (let i = 0; i < sortedTasks.length; i++) {
+      let overlaps = [sortedTasks[i]];
 
-  //   return {
-  //     'grid-row-start': startRow,
-  //     'grid-row-end': endRow
-  //   };
-  // }
+      for (let j = i + 1; j < sortedTasks.length; j++) {
+        if (sortedTasks[j].startTime < sortedTasks[i].endTime) {
+          overlaps.push(sortedTasks[j]);
+        } else {
+          break;
+        }
+      }
+
+      const width = 250 / overlaps.length;
+      overlaps.forEach((task, index) => {
+        task.width = width;
+        task.left = width * index;
+      });
+
+      i += overlaps.length - 1;
+    }
+
+    return sortedTasks;
+  }
 
   getTaskStyle(task: WeekTask): any {
     const startHour = parseInt(task.startTime.split(':')[0], 10);
@@ -77,14 +105,16 @@ export class WeekOverviewComponent {
     const endHour = parseInt(task.endTime.split(':')[0], 10);
     const endMinute = parseInt(task.endTime.split(':')[1], 10);
 
-    const startPosition = (startHour + startMinute / 60) * 50; // Bijvoorbeeld, 9.30 wordt 570 (9*60 + 30)
-    const endPosition = (endHour + endMinute / 60) * 50;
+    const startPosition = (startHour + startMinute / 60) * this.heightPerHour; // Bijvoorbeeld, 9.30 wordt 570 (9*60 + 30)
+    const endPosition = (endHour + endMinute / 60) * this.heightPerHour;
 
     const height = endPosition - startPosition;
 
     return {
       top: `${startPosition}px`, // Hier ga je ervan uit dat elke uur 60px hoog is
-      height: `${height}px`
+      height: `${height}px`,
+      width: `${task.width}px`,
+      left: `${task.left}px`
     };
   }
 }
