8f674912e7c19f9a1f068b9958359b66c645b06c
danielmoss <121039995+danielmosss@users.noreply.github.com>
2024-09-30 15:43:37 +0200

    add migration

---
+++ database/migrations-v2/M1-use-more-ics-imports.sql
@@ -0,0 +1,21 @@
+CREATE TABLE ics_imports
+(
+    id                 INT AUTO_INCREMENT PRIMARY KEY,
+    user_id            INT          NOT NULL,
+    ics_url            VARCHAR(255) NOT NULL,
+    last_imported_at   DATETIME,
+    created_at         DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at         DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    ics_last_synced_at TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT UQ_ics_url UNIQUE (ics_url),
+    CONSTRAINT UQ_ics_url_user_id UNIQUE (ics_url, user_id),
+    CONSTRAINT CK_ics_imports_user_id CHECK (user_id <= 10)
+);
+
+INSERT INTO ics_imports (user_id, ics_url, last_imported_at, created_at, updated_at, ics_last_synced_at)
+SELECT id, webcallurl, null, now(), now(), webcalllastsynced
+FROM users
+where webcallurl is not null;
+
+
